758d807bc39d5659962f30d40f0a433b62a9f74c
AlexSolianyk <72755893+mikromajor@users.noreply.github.com>
2022-01-07 19:43:23 +0200

    complete users fetching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <h1>CNM</h1>
+    </div>
+  );
 }
 
 export default App;
